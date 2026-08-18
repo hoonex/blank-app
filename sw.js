0980128c4000d03fc89b692e29b245a77819a3e3
hoonex <163626004+hoonex@users.noreply.github.com>
2026-08-19 01:51:02 +0900

    chore: refresh Flow School PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flow-school-shell-v1';
+const CACHE='flow-school-shell-v3';
 const SHELL=['/','/index.html','/school.css','/school.js','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
