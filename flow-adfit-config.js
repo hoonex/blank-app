178df9897cf357c9bfdc1114c946b87435a71d0c
hoonex <163626004+hoonex@users.noreply.github.com>
2026-08-25 21:25:21 +0900

    feat: add isolated Kakao AdFit configuration

---
+++ flow-adfit-config.js
@@ -0,0 +1,5 @@
+export const FLOW_ADFIT_CONFIG=Object.freeze({
+  sdk:'https://t1.kakaocdn.net/kas/static/ba.min.js',
+  school:Object.freeze({unit:'',width:320,height:100}),
+  university:Object.freeze({unit:'',width:320,height:100}),
+});
