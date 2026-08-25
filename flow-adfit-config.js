6aa7d92291852c9539df2439849cd83e33300c74
hoonex <163626004+hoonex@users.noreply.github.com>
2026-08-25 23:01:06 +0900

    feat: activate School Kakao AdFit unit

--- flow-adfit-config.js
+++ flow-adfit-config.js
@@ -1,5 +1,5 @@
 export const FLOW_ADFIT_CONFIG=Object.freeze({
   sdk:'https://t1.kakaocdn.net/kas/static/ba.min.js',
-  school:Object.freeze({unit:'',width:320,height:100}),
+  school:Object.freeze({unit:'DAN-ovpJn5XCBs1n1owQ',width:320,height:100}),
   university:Object.freeze({unit:'',width:320,height:100}),
 });
