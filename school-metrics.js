3a7c0f92a1263fc3840833fb4c32e7e9ced9ec10
hoonex <163626004+hoonex@users.noreply.github.com>
2026-08-28 06:42:47 +0900

    feat: load lazy Transit map module

--- school-metrics.js
+++ school-metrics.js
@@ -4,6 +4,7 @@ import '/flow-refraction.js';
 import './school-ia.js';
 import './school-timetable-polish.js';
 import './school-transit.js';
+import './school-transit-map.js';
 
 if(!document.querySelector('link[data-flow-school-settings-wide]')){
   const link=document.createElement('link');
