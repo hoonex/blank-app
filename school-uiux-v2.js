0401315cb5224012a8912365740b80ef03a9e20e
hoonex <163626004+hoonex@users.noreply.github.com>
2026-08-31 02:58:56 +0900

    fix: layer School UI v2 system tokens

--- school-uiux-v2.js
+++ school-uiux-v2.js
@@ -1,10 +1,14 @@
 const root=document.documentElement;
 root.dataset.flowSchoolUi='v2';
 
-if(!document.querySelector('link[data-flow-school-ui-v2]')){
+function attachStyle(href,key){
+  if(document.querySelector(`link[${key}]`))return;
   const link=document.createElement('link');
   link.rel='stylesheet';
-  link.href='/school-uiux-v2.css?v=20260831-1';
-  link.dataset.flowSchoolUiV2='';
+  link.href=href;
+  link.setAttribute(key,'');
   document.head.append(link);
 }
+
+attachStyle('/school-uiux-v2.css?v=20260831-1','data-flow-school-ui-v2');
+attachStyle('/school-uiux-v2-system.css?v=20260831-1','data-flow-school-ui-v2-system');
